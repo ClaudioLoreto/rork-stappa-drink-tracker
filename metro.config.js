142b1a516c6a8c52aeaa720cdbe984b68a92b3b2
Claudio Loreto <claudioloreto@edba20fd-14eb-4b81-a042-d024cf776d3b.fritz.box>
2025-11-18 00:23:23 +0100

    Fix Metro path alias resolver and remove aspectRatio

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,16 @@
 const { getDefaultConfig } = require('expo/metro-config');
+const path = require('path');
 // const { withNativeWind } = require('nativewind/metro');
 
 const config = getDefaultConfig(__dirname);
+
+// Add path alias resolver for @/* imports
+config.resolver = {
+  ...config.resolver,
+  extraNodeModules: {
+    '@': __dirname,
+  },
+};
 
 // Temporarily disable NativeWind to bypass aspect-ratio parse error
 module.exports = config;
