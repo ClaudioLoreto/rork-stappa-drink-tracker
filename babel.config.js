e20efda52c316d005898c2b905a2f0009502546f
Claudio Loreto <claudioloreto@32fe5d64-6aa4-42b4-bf95-b0b9d15bfbc2.fritz.box>
2025-11-18 12:53:56 +0100

    Fix module resolver for TS aliases

--- babel.config.js
+++ babel.config.js
@@ -10,6 +10,7 @@ module.exports = function (api) {
           alias: {
             '@': './',
           },
+          extensions: ['.tsx', '.ts', '.jsx', '.js', '.json']
         },
       ],
     ],
