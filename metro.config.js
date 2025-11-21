9b9ab7dd96ab918d9a2e42f3211b5ca533a50e67
Claudio Loreto <claudioloreto@Mac-mini-di-Claudio.local>
2025-11-21 01:43:28 +0100

    FIX CRASH: Remove global.css import causing fatal exception
    ROOT CAUSE: import '../global.css' in _layout.tsx was being processed by
    nativewind/babel plugin but Metro config has NativeWind disabled, causing
    JavaScript bundle to throw fatal exception on load.
    
    - Remove import '../global.css' from _layout.tsx
    - Disable nativewind/babel plugin in babel.config.js
    - Bump version to 1.0.4
    
    This was causing identical crash across all previous versions (1.0.1, 1.0.2, 1.0.3)
    on com.facebook.react.ExceptionsManagerQueue thread.

--- metro.config.js
+++ metro.config.js
@@ -1,8 +1,6 @@
 const { getDefaultConfig } = require('expo/metro-config');
-// const { withNativeWind } = require('nativewind/metro');
+const { withNativeWind } = require('nativewind/metro');
 
 const config = getDefaultConfig(__dirname);
 
-// Temporarily disable NativeWind to bypass aspect-ratio parse error
-module.exports = config;
-// module.exports = withNativeWind(config, { input: './global.css' });
+module.exports = withNativeWind(config, { input: './global.css' });
